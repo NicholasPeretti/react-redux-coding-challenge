36235dc668e473d7a262055b97a53b8f65bbf385
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-22 23:22:34 +0100

    Upate App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import PresentationalTest from './utils/tests/presentational'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  PresentationalTest(App)
+})
