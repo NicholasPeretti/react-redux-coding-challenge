126c834d8e644a48cdc3f288570e0175599fead5
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-22 23:10:50 +0100

    Create style context

---
+++ src/style.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    accent: '#EA7F28',
+    accentPressed: '#D37324',
+    textColor: '#4A4A4A',
+    gray: '#EDEDED'
+  },
+  spacing: {
+    padding1: '8px',
+    padding2: '12px',
+    padding3: '24px'
+  }
+}
---
+++ src/styleContext.js
@@ -0,0 +1,4 @@
+import style from './style'
+import { createContext } from 'react'
+
+export default createContext(style)
