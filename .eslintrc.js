28c70dcb5567d93e9de0cfff686600fdbda2c498
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-22 22:59:25 +0100

    Init .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": ["standard", "plugin:jest/recommended"],
+    "plugins": ["react"],
+    "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars":
+        "error"
+   }
+};
