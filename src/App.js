c538315460393717c643e1612a3025ce3ae11f60
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-22 23:19:54 +0100

    Create App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import StyleContext from './styleContext'
+import style from './style'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <StyleContext.Provider value={style}>
+    <StyleContext.Consumer>
+      {context => (
+        <div
+          style={{
+            color: context.colors.textColor
+          }}
+        >
+          <header>
+            <Navbar />
+          </header>
+          <main
+            style={{
+              minHeight: window.innerHeight - 165
+            }}
+          />
+          <Footer />
+        </div>
+      )}
+    </StyleContext.Consumer>
+  </StyleContext.Provider>
+)
 
-export default App;
+export default App
