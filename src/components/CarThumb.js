7b51eb4b88be9555c62fb22590b462a9d6749b40
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 15:56:24 +0100

    Replace StyleContext with styled-component in CarThumb

--- src/components/CarThumb.js
+++ src/components/CarThumb.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import StyleContext from '../styleContext'
+import Style from '../style'
+import styled from 'styled-components'
+
+const StyledImage = styled.img`
+  width: 100px;
+  height: 80px;
+  margin: ${Style.spacing.padding2};
+`
 
 var CarThumb = ({ url }) => (
-  <StyleContext.Consumer>
-    {context => {
-      const style = {
-        width: '100px',
-        height: '80px',
-        margin: context.spacing.padding2
-      }
-      return (
-        <img src={url} style={style} />
-      )
-    }}
-  </StyleContext.Consumer>
+  <StyledImage src={url} />
 )
 
 CarThumb.propTypes = {
