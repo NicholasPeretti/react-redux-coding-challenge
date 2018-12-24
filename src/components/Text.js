c55dbb5ee880e987622d0177d7429d743df003e3
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 17:07:11 +0100

    Text code refactoring

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,16 +1,9 @@
-import React from 'react'
 import styled from 'styled-components'
 
-const StyledSpan = styled.span`
+const Text = styled.span`
   font-size: 14px;
   font-weight: normal;
   margin: 0px;
 `
-
-const Text = ({ children, style = {} }) => (
-  <StyledSpan style={style}>
-    {children}
-  </StyledSpan>
-)
 
 export default Text
