977d424c4e7b322bb02240fbe4062af5f97f9e27
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 17:17:10 +0100

    Replace StyleContext with styled-component in NotFoundMessage

--- src/components/NotFoundMessage.js
+++ src/components/NotFoundMessage.js
@@ -3,32 +3,29 @@ import NavLogo from './NavLogo'
 import Title from './Title'
 import SubTitle from './SubTitle'
 import Link from './Link'
-import StyleContext from '../styleContext'
+import Style from '../style'
+import styled from 'styled-components'
 
-const style = {
-  display: 'flex',
-  justifyContent: 'center',
-  textAlign: 'center',
-  flexDirection: 'column'
-}
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  flex-direction: column;
+`
+
+const StyledSubTitle = styled(SubTitle)`
+  margin: ${Style.spacing.padding3} 0px;
+`
 
 const NotFoundMessage = () => (
-  <StyleContext.Consumer>
-    {context => (
-      <div style={style}>
-        <NavLogo/>
-        <Title>404 - Not Found</Title>
-        <SubTitle
-          style={{
-            marginBottom: context.spacing.padding3,
-            marginTop: context.spacing.padding3
-          }}>
-            Sorry, the page you are looking for does not exist.
-        </SubTitle>
-        <SubTitle>You can always go back to the <Link>homepage</Link>.</SubTitle>
-      </div>
-    )}
-  </StyleContext.Consumer>
+  <StyledContainer>
+    <NavLogo/>
+    <Title>404 - Not Found</Title>
+    <StyledSubTitle>
+        Sorry, the page you are looking for does not exist.
+    </StyledSubTitle>
+    <SubTitle>You can always go back to the <Link>homepage</Link>.</SubTitle>
+  </StyledContainer>
 )
 
 export default NotFoundMessage
