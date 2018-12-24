d4fbda8551192c884b3e8cae95c82fbc6835d145
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 17:01:31 +0100

    SubTitle code refactoring

--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -1,16 +1,9 @@
-import React from 'react'
 import styled from 'styled-components'
 
-const StyledH3 = styled.h3`
+const SubTitle = styled.h3`
   margin: 0px;
   font-size: 18px;
   font-weight: normal;
 `
-
-const SubTitle = ({ children, style = {} }) => (
-  <StyledH3 style={style}>
-    {children}
-  </StyledH3>
-)
 
 export default SubTitle
