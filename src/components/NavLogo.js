18ce0d1c059f9bdb91a77a9178ac9bbdbc84c01a
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 11:01:27 +0100

    Add wrapper flexbox to NavLogo

--- src/components/NavLogo.js
+++ src/components/NavLogo.js
@@ -3,18 +3,25 @@ import StyleContext from '../styleContext'
 import logo from '../logo.png'
 
 var NavLogo = () => (
-  <StyleContext.Consumer>
-    {context => (
-      <img
-        src={logo}
-        alt="Auto1"
-        style={{
-          maxHeight: '30px',
-          margin: `${context.spacing.padding3} 0px`
-        }}
-      />
-    )}
-  </StyleContext.Consumer>
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center'
+    }}
+  >
+    <StyleContext.Consumer>
+      {context => (
+        <img
+          src={logo}
+          alt="Auto1"
+          style={{
+            maxHeight: '30px',
+            margin: `${context.spacing.padding3} 0px`
+          }}
+        />
+      )}
+    </StyleContext.Consumer>
+  </div>
 )
 
 export default NavLogo
