6b478fe476b008477c35c35d2e0dc3eb65cf9e96
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 16:11:14 +0100

    Replace StyleContext with styled-component in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react'
-import StyleContext from '../styleContext'
+import React from 'react'
 import NavLogo from './NavLogo'
 import NavMenu from './NavMenu'
 import NavMenuItem from './NavMenuItem'
+import Style from '../style'
+import styled from 'styled-components'
 
-class Navbar extends Component {
-  render () {
-    return (
-      <StyleContext.Consumer>
-        {context => (
-          <header>
-            <nav
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                height: '80px',
-                borderBottom: `1px solid ${context.colors.gray}`,
-                padding: `0px ${context.spacing.padding3}`
-              }}
-            >
-              <NavLogo />
-              <NavMenu>
-                <NavMenuItem>Purchase</NavMenuItem>
-                <NavMenuItem>My Orders</NavMenuItem>
-                <NavMenuItem>Sell</NavMenuItem>
-              </NavMenu>
-            </nav>
-          </header>
-        )}
-      </StyleContext.Consumer>
-    )
-  }
-}
+const StyledNav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 80px;
+  border-bottom: 1px solid ${Style.colors.gray};
+  padding: 0px ${Style.spacing.padding3};
+`
+
+const Navbar = () => (
+  <header>
+    <StyledNav>
+      <NavLogo />
+      <NavMenu>
+        <NavMenuItem>Purchase</NavMenuItem>
+        <NavMenuItem>My Orders</NavMenuItem>
+        <NavMenuItem>Sell</NavMenuItem>
+      </NavMenu>
+    </StyledNav>
+  </header>
+)
 
 export default Navbar
