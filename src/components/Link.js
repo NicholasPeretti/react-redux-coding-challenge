bc89974fb5623a5376179e80da8e09b119ba7f65
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-24 15:41:26 +0100

    Replace StyleContext with styled-component in Link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import StyleContext from '../styleContext'
+import Style from '../style'
+import styled from 'styled-components'
 
-var Link = ({ to, style, children }) => (
-  <StyleContext.Consumer>
-    {context => (
-      <a
-        href={to}
-        style={{
-          color: context.colors.accent,
-          ...style
-        }}
-      >
-        {children}
-      </a>
-    )}
-  </StyleContext.Consumer>
+const StyledLink = styled.a`
+  color: ${Style.colors.accent};
+  text-decoration: none;
+
+  &:hover: {
+    text-decoration: underline;
+  }
+`
+
+var Link = ({ to, children }) => (
+  <StyledLink href={to}>
+    {children}
+  </StyledLink>
 )
 
 Link.propTypes = {
