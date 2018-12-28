ef806443646a36d74427cccaa85c468a4c965f6f
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-28 01:05:32 +0100

    Create shared mock data file to use in tests

---
+++ src/utils/tests/data.js
@@ -0,0 +1,126 @@
+export const CarsList = [
+  {
+    stockNumber: 10079,
+    manufacturerName: 'BMW',
+    modelName: 'X1',
+    color: 'black',
+    mileage: {
+      number: 178994,
+      unit: 'km'
+    },
+    fuelType: 'Diesel',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 10206,
+    manufacturerName: 'Skoda',
+    modelName: 'Octavia',
+    color: 'black',
+    mileage: {
+      number: 142074,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 10384,
+    manufacturerName: 'Dodge',
+    modelName: 'Journey',
+    color: 'green',
+    mileage: {
+      number: 125976,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 10556,
+    manufacturerName: 'Mercedes-Benz',
+    modelName: 'CLC-Klasse',
+    color: 'white',
+    mileage: {
+      number: 144895,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 10577,
+    manufacturerName: 'Skoda',
+    modelName: 'Felicia',
+    color: 'blue',
+    mileage: {
+      number: 173054,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 10819,
+    manufacturerName: 'Volkswagen',
+    modelName: 'Golf I',
+    color: 'black',
+    mileage: {
+      number: 179730,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 10908,
+    manufacturerName: 'Mercedes-Benz',
+    modelName: 'GT AMG',
+    color: 'green',
+    mileage: {
+      number: 150110,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 11160,
+    manufacturerName: 'Dodge',
+    modelName: 'Avenger',
+    color: 'yellow',
+    mileage: {
+      number: 130487,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 11281,
+    manufacturerName: 'Dodge',
+    modelName: 'Avenger',
+    color: 'white',
+    mileage: {
+      number: 184601,
+      unit: 'km'
+    },
+    fuelType: 'Diesel',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  },
+  {
+    stockNumber: 11290,
+    manufacturerName: 'Tesla',
+    modelName: 'Model S',
+    color: 'green',
+    mileage: {
+      number: 187797,
+      unit: 'km'
+    },
+    fuelType: 'Petrol',
+    pictureUrl: 'http://localhost:3001/car.svg'
+  }
+]
+
+export default {
+  CarsList
+}
