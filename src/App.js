c9e0cea44065775d21b9370d6a97839cf0a732a4
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-28 11:58:18 +0100

    Add router in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Style from './style'
 import styled from 'styled-components'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import PageNotFound from './screens/PageNotFound'
 
 const AppContainer = styled.div`
   color: ${Style.colors.textColor};
@@ -18,6 +20,12 @@ const App = () => (
     <AppContainer>
       <Navbar />
       <StyledMain>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/' exact render={() => (<h1>Home</h1>)} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
       </StyledMain>
       <Footer />
     </AppContainer>
