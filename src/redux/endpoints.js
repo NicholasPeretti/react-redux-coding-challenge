89e3179fa12af51d9543363ca531f5457c0c03c9
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-29 23:44:12 +0100

    Add GET_COLORS endpoint

--- src/redux/endpoints.js
+++ src/redux/endpoints.js
@@ -5,6 +5,12 @@ export const GET_CARS = {
   method: 'GET'
 }
 
+export const GET_COLORS = {
+  url: `${API_SERVER}/colors`,
+  method: 'GET'
+}
+
 export default {
-  GET_CARS
+  GET_CARS,
+  GET_COLORS
 }
