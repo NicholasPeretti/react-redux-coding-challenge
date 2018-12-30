94ab94245099cf9680218c383e96e5a6f31b699d
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-31 00:07:37 +0100

    Add Car Middleware to the App

--- src/redux/middleware/index.js
+++ src/redux/middleware/index.js
@@ -2,6 +2,7 @@ import api from './core/api'
 import cars from './app/cars'
 import colorsFilter from './app/colorsFilter'
 import manufacturersFilter from './app/manufacturersFilter'
+import car from './app/car'
 
 const CORE_MIDDLEWARES = [ api ]
 
@@ -9,5 +10,6 @@ export default [
   ...CORE_MIDDLEWARES,
   cars,
   colorsFilter,
-  manufacturersFilter
+  manufacturersFilter,
+  car
 ]
