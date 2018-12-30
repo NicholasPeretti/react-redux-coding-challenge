1fe40e15ec07a29313b079fe76f8a8f4287b7d12
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-30 14:00:32 +0100

    Add manufacturersFilter middleware to the App

--- src/redux/middleware/index.js
+++ src/redux/middleware/index.js
@@ -1,11 +1,13 @@
 import api from './core/api'
 import cars from './app/cars'
 import colorsFilter from './app/colorsFilter'
+import manufacturersFilter from './app/manufacturersFilter'
 
 const CORE_MIDDLEWARES = [ api ]
 
 export default [
   ...CORE_MIDDLEWARES,
   cars,
-  colorsFilter
+  colorsFilter,
+  manufacturersFilter
 ]
