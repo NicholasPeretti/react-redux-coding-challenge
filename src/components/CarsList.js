de2a214c726fcb8a7d51f7830660ccf2ee267d4f
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-12-31 01:02:50 +0100

    Add Empty list message in CarsList

--- src/components/CarsList.js
+++ src/components/CarsList.js
@@ -3,23 +3,38 @@ import PropTypes from 'prop-types'
 import CarsListItem from './CarsListItem'
 import styled from 'styled-components'
 import CarPropType from '../utils/propTypes/car'
+import SubTitle from './Title'
 
 const ListContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
 `
+const Center = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`
 
-var CarsList = ({ cars }) => (
-  <ListContainer>
-    {cars.map((car, key) => <CarsListItem car={car} key={key}/>)}
-  </ListContainer>
-)
+var CarsList = ({ cars }) => {
+  if (!cars.length) {
+    return (
+      <Center>
+        <SubTitle>No cars found</SubTitle>
+      </Center>
+    )
+  }
+  return (
+    <ListContainer>
+      {cars.map((car, key) => (
+        <CarsListItem car={car} key={key} />
+      ))}
+    </ListContainer>
+  )
+}
 
 CarsList.propTypes = {
-  cars: PropTypes.arrayOf(
-    CarPropType
-  ).isRequired
+  cars: PropTypes.arrayOf(CarPropType).isRequired
 }
 
 export default CarsList
